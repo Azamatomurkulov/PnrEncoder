afee5f91d29d209673b30a945447353516fd8454
aiairv <a.a.kubatova@gmail.com>
2023-03-17 13:15:22 +0600

    commit6

--- src/main/resources/db.migration/V2__create_admin_and_moderator.sql
+++ src/main/resources/db.migration/V2__create_admin_and_moderator.sql
@@ -1,2 +1,3 @@
---INSERT INTO users_table VALUES ('id', 'email', 'full_name', 'login', 'password', 'rdt', 'role')
---    (1,'azam',)
+INSERT INTO users_table (id, email, full_name, login, password, rdt, role) VALUES
+        (1, 'azamatomurkulov@gmail.com', 'Azamat Omurkulov', 'admin', 'admin', null, 'ADMIN'),
+        (2, 'a.a.kubatova@gmail.com', 'Aidai Kubatova', 'moderator', 'moderator', null, 'MODERATOR');
